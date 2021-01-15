ee5db9edd299aa1bf2331bb342e7433788fc46d3
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-01-15 15:47:11 -0300

    refatorando

--- src/App.js
+++ src/App.js
@@ -2,81 +2,9 @@ import React, { Component } from 'react';
 import Input from './components/Text/Input';
 import Transformation from './components/Text/Transformation';
 import Transformations from './components/Text/Transformations';
-import * as stringHelpers from './helpers/stringHelpers';
+import { defaultState, MY_TRANSFORMATIONS } from './states/app.states';
 
-const MY_TRANSFORMATIONS = [
-  {
-    id: 't1',
-    description: 'Texto invertido:',
-    transformFunction: text => text.split('').reverse().join(''),
-  },
-  {
-    id: 't2',
-    description: 'CSV:',
-    transformFunction: text => text.split(' ').map(word => `"${word}"`).join(';'),
-  },
-  {
-    id: 't3',
-    description: 'Texto numérico:',
-    transformFunction: text =>
-      stringHelpers
-        .removeSpecialCharacteres(text)
-        .toUpperCase()
-        .split('')
-        .map(char => {
-          if (char === 'O') return '0';
-          if (char === 'A') return '4';
-          if (char === 'E') return '3';
-          if (char === 'I') return '1';
-          if (char === 'S') return '5';
-          if (char === 'T') return '7';
 
-          return char;
-        })
-        .join(''),
-  },
-  {
-    id: 't4',
-    description: 'Slug:',
-    transformFunction: text =>
-      stringHelpers.removeSpecialCharacteres(text).toLowerCase().split(' ').join('_'),
-  },
-  {
-    id: 't5',
-    description: 'Somente vogais:',
-    transformFunction: text =>
-      text.split('').filter(char => char === ' ' || stringHelpers.isVowel(char)).join(''),
-  },
-  {
-    id: 't6',
-    description: 'Somente consoantes:',
-    transformFunction: text =>
-      text.split('').filter(char => char === ' ' || stringHelpers.isConsonant(char)).join(''),
-  },
-  {
-    id: 't7',
-    description: 'Variável (CamelCase):',
-    transformFunction: text =>
-      stringHelpers.removeSpecialCharacteres(text).toLowerCase().split(' ').map((word, index) => {
-        return index === 0 ? word : word.split('').map((char, index) => {
-          return index === 0 ? char.toUpperCase() : char;
-        }).join('');
-      }).join(''),
-  },
-  {
-    id: 't8',
-    description: 'Alternância entre maiúsculas e minúsculas:',
-    transformFunction: text =>
-      text
-        .split('')
-        .map((char, index) => index % 2 === 0 ? char.toUpperCase() : char.toLowerCase())
-        .join(''),
-  },
-]
-
-const defaultState = {
-  userInput: ''
-};
 
 export default class App extends Component {
 
